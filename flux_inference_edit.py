bff374a355b75104674cbc2c5f2c7c87d1bf7c9f
Andreas Jörg <andreasjoerg@MBPvonAndreas2.fritz.box>
2025-04-07 19:33:24 +0200

    typo

--- flux_inference_edit.py
+++ flux_inference_edit.py
@@ -18,7 +18,7 @@ mask = Image.new("RGB", (canvas_width, canvas_height), "white")
 
 # Setup and configure the FluxFill pipeline as before
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-pipe = FluxFillEditPipeline.from_pretrained("black-forest-labs/FLUX.1-Fill-dev").to("device")
+pipe = FluxFillEditPipeline.from_pretrained("black-forest-labs/FLUX.1-Fill-dev").to(device)
 pipe.load_lora_weights("flux-edit_circles-lora/checkpoint-500/pytorch_lora_weights.safetensors")
 pipe.fuse_lora(lora_scale=1.0)
 
